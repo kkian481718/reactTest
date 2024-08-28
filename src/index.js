99578431270772320454ae7479e108819724eba8
Miffy <39194102+kkian481718@users.noreply.github.com>
2024-08-28 14:17:43 +0800

    add nav links

--- src/index.js
+++ src/index.js
@@ -5,12 +5,27 @@ import { HashRouter, Routes, Route, Link } from "react-router-dom";
 import MenuPage from "./page/MenuPage";
 import FormPage from "./page/FormPage";
 
+const Layout = (props) => {
+  return (
+    <>
+      <nav>
+        <Link to="/">點我連到第一頁</Link>
+        <Link to="/form" style={{ marginLeft: "20px" }}>
+          點我連到第二頁
+        </Link>
+      </nav>
+      {props.children}
+    </>
+  );
+};
+
 const App = () => {
   return (
     <HashRouter>
+      <Layout/>
       <Routes>
-        <Route path="/" element={<MenuPage />} />
-        <Route path="/form" element={<FormPage />} />
+          <Route path="/" element={<MenuPage />} />
+          <Route path="/form" element={<FormPage />} />
       </Routes>
     </HashRouter>
   );
