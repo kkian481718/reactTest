5eae2c8b9c44ac7b648e9cfdfb1e6241608f253b
Miffy <39194102+kkian481718@users.noreply.github.com>
2024-08-28 11:52:50 +0800

    實作useState和useEffect

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -1,45 +1,63 @@
-import React from 'react';
- 
+import React, { useState, useEffect } from "react";
+
 const menuContainerStyle = {
-    position: 'relative',
-    width: '300px',
-    padding: '14px',
-    fontFamily: 'Microsoft JhengHei',
-    paddingBottom: '7px',
-    backgroundColor: 'white',
-    border: '1px solid #E5E5E5',
+  position: "relative",
+  width: "300px",
+  padding: "14px",
+  fontFamily: "Microsoft JhengHei",
+  paddingBottom: "7px",
+  backgroundColor: "white",
+  border: "1px solid #E5E5E5",
 };
- 
+
 const menuTitleStyle = {
-    marginBottom: '7px',
-    fontWeight: 'bold',
-    color: '#00a0e9',
-    cursor: 'pointer',
+  marginBottom: "7px",
+  fontWeight: "bold",
+  color: "#00a0e9",
+  cursor: "pointer",
 };
- 
+
 const menuBtnStyle = {
-    position: 'absolute',
-    right: '7px',
-    top: '33px',
-    backgroundColor: 'transparent',
-    border: 'none',
-    color: '#00a0e9',
-    outline: 'none',
+  position: "absolute",
+  right: "7px",
+  top: "33px",
+  backgroundColor: "transparent",
+  border: "none",
+  color: "#00a0e9",
+  outline: "none",
 };
- 
+
 const menuStyle = {
-    display: 'block', //這裡先讓它顯示
+  display: "block", //這裡先讓它顯示
 };
- 
-function Menu(props) {
-    return (
-        <div style={menuContainerStyle}>
-            <p style={menuTitleStyle}>{props.title}</p>
-            <button style={menuBtnStyle}>V</button>
-            {/* <ul style={menuStyle}>{props.menuItems}</ul> */}
-            <ul style={menuStyle}>{props.children}</ul>
-        </div>
-    );
+
+function Menu({ title, children }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (isOpen) {
+      document.getElementsByClassName("menu-btn")[0].textContent = "^";
+      document.getElementsByClassName("menu")[0].style.display = "block";
+    } else {
+      document.getElementsByClassName("menu-btn")[0].textContent = "V";
+      document.getElementsByClassName("menu")[0].style.display = "none";
+    }
+  }, [isOpen]);
+
+  return (
+    <div style={menuContainerStyle}>
+      <p style={menuTitleStyle}>{title}</p>
+      <button
+        style={menuBtnStyle}
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        V
+      </button>
+      {isOpen && <ul>{children}</ul>}
+    </div>
+  );
 }
- 
+
 export default Menu;
